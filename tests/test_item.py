681c9134ab07d1427b2738ad9bb83419c9cd9a29
Artem Mironenkov <mir.art1998@gmail.com>
2023-09-04 16:11:23 +0500

    HW_1 + tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import src.item as item
+
+if __name__ == '__main__':
+    item.Item.pay_rate = 0,5
+    item1 = item.Item("Смартфон", 20000, 30)
+    # testing calculate_total_price
+    assert item1.calculate_total_price() == 600000
+    # testing apply_discount
+    item1.apply_discount()
+    assert item1.price == 20000 * item1.pay_rate
